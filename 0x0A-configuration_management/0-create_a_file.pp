6bd9fae120094da5c5994165dafb9086b0bccafb
felixokolo <pheelix.info@gmail.com>
2022-09-16 20:26:23 +0000

    correction

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
 # creat a file
 file { 'school':
-  path  => '/tmp/school',
-  owner => 'www-data',
-  group => 'www-data'
+  path    => '/tmp/school',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
