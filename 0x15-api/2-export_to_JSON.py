e4093f797eb4c5c52ad81801d37513f092c5b126
felixokolo <pheelix.info@gmail.com>
2022-10-18 04:26:20 +0000

    addition

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""API testing module"""
+import csv
+import json
+import sys
+from urllib import request
+
+"""API request for user todo list"""
+if __name__ == '__main__':
+    if (len(sys.argv) < 2):
+        exit()
+    api_url_base = 'https://jsonplaceholder.typicode.com/'
+    req_url = api_url_base + 'users/' + sys.argv[1]
+    user_details = json.loads(request.urlopen(req_url).read().
+                              decode('utf-8'))
+    uname = user_details.get("username")
+    uid = user_details.get('id')
+    req_url = api_url_base + 'todos?userId=' + sys.argv[1]
+    response = request.urlopen(req_url)
+    resp = response.read().decode('utf-8')
+    resp_list = json.loads(resp)
+    emp_rec = {str(uid): [{'task': x.get('title'),
+                           'completed': x.get('completed'),
+                            'username': uname} for x in resp_list]}
+    with open(str(uid) + '.json', 'w') as jsonwriter:
+        json.dump(emp_rec, jsonwriter)
