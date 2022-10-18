beb5be10c12245635a71f27317b5ab3280490bd6
felixokolo <pheelix.info@gmail.com>
2022-10-18 04:01:40 +0000

    correction

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -21,10 +21,10 @@ if __name__ == '__main__':
     resp_list = json.loads(resp)
     done_list = [x for x in resp_list if x.get('completed')]
     emp_rec = [[str(uid), str(uname),
-                istr(x.get('completed')).lower(),
+                str(x.get('completed')).lower(),
                 x.get('title')] for x in resp_list]
     with open(str(uid) + '.csv', 'w', newline='') as csvfile:
-        csv_writer = csv.writer(csvfile, delimiter=' ',
+        csv_writer = csv.writer(csvfile, delimiter=',',
                                 quoting=csv.QUOTE_ALL)
         for row in emp_rec:
             csv_writer.writerow(row)
